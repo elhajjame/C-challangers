b45cd013331de44f34d2bccd3bd71b6cb03f2c3b
mehdi el-hajjame <el-hajjame@outlook.com>
2025-09-22 23:02:12 +0100

    first commit

---
+++ dayOne/variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    /*
+    Challenge 1: Display Information
+Write a C program that displays your personal information: 
+last name, first name, age, gender, and email address. The data is entered from the keyboard.
+    */
+
+
+    return 0;
+}

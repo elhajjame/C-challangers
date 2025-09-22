5ff013c6bde41fc02ebb4fddfb2ba0d4de6693cc
mehdi el-hajjame <el-hajjame@outlook.com>
2025-09-22 23:57:03 +0100

    creating variables and user inputs

--- dayOne/variables.c
+++ dayOne/variables.c
@@ -5,7 +5,28 @@ int main(){
 Write a C program that displays your personal information: 
 last name, first name, age, gender, and email address. The data is entered from the keyboard.
     */
+   char lastName [50];
+    char firstName [50];
+    int age;
+    char gender [6];
+    char email [50];
 
+printf("type your last name");
+scanf("%s", &lastName);
+
+printf("type your first name");
+scanf("%s", &firstName);
+
+printf("enter your age");
+scanf("%d", &age);
+
+printf("select your gender");
+scanf("%s", &gender);
+
+printf("enter email");
+scanf("%s", &email);
+
+    
 
     return 0;
 }
